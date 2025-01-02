78b6e588e3436297817a169d6252c546a2b37dcf
Kishor Patil <kishor.patil@qodequay.in>
2025-01-02 19:26:57 +0530

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import HomeScreen from "@/src/screens/HomeScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import MainNavigation from '../src/navigation/MainNavigation'
+
+export default function Index() {
+  return (
+    // <NavigationContainer  >
+    //   <MainNavigation />
+    // </NavigationContainer>
+    <HomeScreen />
+  );
+}
---
+++ src/navigation/MainNavigation.js
@@ -0,0 +1,16 @@
+import { View, Text } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import React from "react";
+import HomeScreen from "../screens/HomeScreen";
+const Stack = createStackNavigator();
+
+export default function MainNavigation() {
+  return (
+    <Stack.Navigator
+      initialRouteName={HomeScreen}
+      screenOptions={{ headerShown: false }}
+    >
+      <Stack.Screen name="HomeScreen" component={HomeScreen} />
+    </Stack.Navigator>
+  );
+}
